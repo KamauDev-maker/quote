650d01a768377197b5d9bcb764b8f9f9e29b2e69
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-07 23:10:35 +0300

    creating both output binding & Event

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -16,6 +16,11 @@ export class QuotesComponent implements OnInit {
     new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.'),
     new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.'),
   ]
+  get sortQuotes() {
+    return this.quotes.sort((a, b) => {
+
+    });
+  }
 
 
   constructor() { }
