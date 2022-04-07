9f3a87da858656f1e7fe5c999bc221ee765cce7f
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-07 23:57:03 +0300

    adding the date

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
   title = 'empowering quotes'
   quotes:Quote[] = [
-    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.'),
-    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.'),
-    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.'),
-    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.'),
-    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.'),
-    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.'),
+    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.',new Date(2020,0,12)),
+    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.',new Date(2019,11,18)),
+    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.',new Date(2021,6,30)),
+    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.',new Date(2018,7,12)),
+    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.',new Date(2017,3,28)),
+    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.',new Date(2022,3,12)),
   ]
   get sortQuotes() {
     return this.quotes.sort((a, b) => {
