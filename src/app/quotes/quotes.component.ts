1603ac38a17fb472d721d2c39db1de335cc8b1e7
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-10 00:27:37 +0300

    adding a README.md

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
   title = 'empowering quotes'
   quotes:Quote[] = [
-    new Quote (1,'Marvin Kimathi','“Think like a queen. A queen is not afraid to fail. Failure is another stepping stone to greatness.”','Oprah Winfrey',new Date(2021,11,12),3,0),
-    new Quote (2,'Haimana Utu','“Never be ashamed of what you feel. You have the right to feel any emotion that you want, and to do what makes you happy. That’s my life motto.”','Demi Levato',new Date(2022,0,16),0,0),
-    new Quote (3,'Andre Leon','“In life there is no real safety except for self-belief.”','Madonna',new Date(2021,6,08),0,0),
-    new Quote (4,'Davine Phyllis','“I think that passion is the secret ingredient that drives hard work and excellence.”','Kelly Ayotte',new Date(2020,10,18),0,0),
-    new Quote (5,'Oscar Kamau','“The man who does not read books has no advantage over the one who cannot read them.”','Mark Twain',new Date(2021,8,22),0,0),
-    new Quote (6,'Shalyne Waweru','Accept what is, let go of what was and have faith in what will be.','John Doe',new Date(2021,11,12),0,0)
+    new Quote (1,'Marvin','"Accept what is, let go of what was and have faith in what will be."','Forte',new Date(2021,0,12),2,0),
+    new Quote (2,'Shalyne','“Learn as if you will live forever, live like you will die tomorrow.”.','Mahatma Gandhi',new Date(2021,8,12),10,0),
+    new Quote (3,'Andre','“When you change your thoughts, remember to also change your world.”.','Norman Vincent Peale',new Date(2020,0,12),0,3),
+    new Quote (4,'Davine','“The only way of discovering the limits of the possible is to venture a little way past them into the impossible.” ','Arthur C. Clarke',new Date(2022,0,12),0,0),
+    new Quote (5,'Natalie Kabaria','"The power of a positive thinking is like an engine that can take you to the summit of a mountain”','Natalia Kabaria',new Date(2022,3,9),0,0),
+    new Quote (6,'Haimana','“If you don’t risk anything, you risk even more.”','John Doe',new Date(2022,0,1),0,0)
   ]
   get sortQuotes() {
     return this.quotes.sort((a, b) => {
