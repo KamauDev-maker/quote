5dc883f2cdcab4c17dc8ccd1131afd9eb1a83f4d
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-09 22:05:32 +0300

    ~rewriting the quotes & styling

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
   title = 'empowering quotes'
   quotes:Quote[] = [
-    new Quote (1,'Jane Doe','Accept what is, let go of what was and have faith in what will be.','John Doe',new Date(2020,0,12),2,0),
-    new Quote (2,'Jane Doe','Accept what is, let go of what was and have faith in what will be.','John Doe',new Date(2020,0,12),0,0),
-    new Quote (3,'Jane Doe','Accept what is, let go of what was and have faith in what will be.','John Doe',new Date(2020,0,12),0,0),
-    new Quote (4,'Jane Doe','Accept what is, let go of what was and have faith in what will be.','John Doe',new Date(2020,0,12),0,0),
-    new Quote (5,'Jane Doe','Accept what is, let go of what was and have faith in what will be.','John Doe',new Date(2020,0,12),0,0),
-    new Quote (6,'Jane Doe','Accept what is, let go of what was and have faith in what will be.','John Doe',new Date(2020,0,12),0,0)
+    new Quote (1,'Marvin Kimathi','“Think like a queen. A queen is not afraid to fail. Failure is another stepping stone to greatness.”','Oprah Winfrey',new Date(2021,11,12),3,0),
+    new Quote (2,'Haimana Utu','“Never be ashamed of what you feel. You have the right to feel any emotion that you want, and to do what makes you happy. That’s my life motto.”','Demi Levato',new Date(2022,0,16),0,0),
+    new Quote (3,'Andre Leon','“In life there is no real safety except for self-belief.”','Madonna',new Date(2021,6,08),0,0),
+    new Quote (4,'Davine Phyllis','“I think that passion is the secret ingredient that drives hard work and excellence.”','Kelly Ayotte',new Date(2020,10,18),0,0),
+    new Quote (5,'Oscar Kamau','“The man who does not read books has no advantage over the one who cannot read them.”','Mark Twain',new Date(2021,8,22),0,0),
+    new Quote (6,'Shalyne Waweru','Accept what is, let go of what was and have faith in what will be.','John Doe',new Date(2021,11,12),0,0)
   ]
   get sortQuotes() {
     return this.quotes.sort((a, b) => {
